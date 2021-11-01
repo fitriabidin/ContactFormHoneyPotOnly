e8847a5a0b118d72ffd78f09631ed69f6417674e
Fitri Abidin <59369184+fitriabidin@users.noreply.github.com>
2021-11-01 09:16:19 +0800

    Update action.php

--- action.php
+++ action.php
@@ -4,7 +4,7 @@ if (isset($_POST['submit'])) {
     session_start();
 
 	// Handling Honey Pot
-    if (isset($_POST['first_name']) && !empty($_POST['first_name']) ) {
+     if ((isset($_POST['first_name']) && !empty($_POST['first_name'])) || (isset($_POST['phone']) && !empty($_POST['phone'])) || (isset($_POST['emails']) && !empty($_POST['emails'])) || (isset($_POST['messages']) && !empty($_POST['messages'])) ) {
         echo "This Is spammer, Dont Process The form";
 		// redirect with succes to trick the spammer
     } else {
